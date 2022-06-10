5576a517560acbceefa09f56bb4bca77219bc954
chrispus1000 <minrevac@gmail.com>
2022-06-10 16:58:30 +0300

    commit tree 4d37a15e5040355be5af57b48014e09a77cfd196

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+function findMatching(driversArray, nameToFind){
+    //const filtered = drivers.filter(name => name.drivers === "Bobby" && "bobby")
+    const filtered = driversArray.filter(name => nameToFind.toLowerCase() === name.toLowerCase() )
+     //console.log(filtered)
+    return filtered
+    
+  }
+
+  function fuzzyMatch(driversArray, lettersToFind){
+    const filtered = driversArray.filter(driverNames => {
+    
+     return driverNames.toLowerCase().startsWith(lettersToFind.toLowerCase());
+      
+      //console.log('driverNames', driverNames)
+      //console.log('lettersTofind', lettersToFind)
+      
+    })
+    return filtered
+  }
+
+  function matchName(driversArray, nameToFind){
+    const filtered = driversArray.filter(object => nameToFind.toLowerCase() === object.name.toLowerCase() )
+    
+      return filtered
+  }
+ 
+//console.log('namesToFind', namesToFind)
